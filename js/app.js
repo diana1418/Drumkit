f0ecc7f30dc8b116a1f61e4b304b380fd4240385
diana1418 <diana_raquelhm1418@hotmail.com>
2020-09-11 00:41:23 -0500

    new method

--- js/app.js
+++ js/app.js
@@ -1,18 +1,61 @@
-
-function playSound(e) {
-  const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-  const key = document.querySelector('.key[data-key="${e.keyCode}"]');
-  if (!audio) return; // stop the function from running all together
-  audio.currentTime = 0; // rewind to the start
-  audio.play();
-  key.classList.add('playing');
+var data = {
+  'A': { name: 'Clap', sound: 'sounds/clap.wav'},
+  'S': { name: 'Hihat', sound: 'sounds/hihat.wav'},
+  'D': { name: 'Kick', sound: 'sounds/kick.wav'},
+  'F': { name: 'Openhat', sound: 'sounds/openhat.wav'},
+  'G': { name: 'Boom', sound: 'sounds/boom.wav'},
+  'H': { name: 'Ride', sound: 'sounds/ride.wav'},
+  'J': { name: 'Snare', sound: 'sounds/snare.wav'},
+  'K': { name: 'Tom', sound: 'sounds/tom.wav'},
+  'L': { name: 'Tink', sound: 'sounds/tink.wav'},
 };
 
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return; // skip it if it's not a transform
-  this.classList.remove('playing');
+var drumkit = document.getElementById('drumkit');
+
+function construct() {
+  for (var key in data) {
+    var drumEl = document.createElement('div');
+    drumEl.classList.add('drum');
+    
+    var h2 = document.createElement('h2');
+    h2.textContent = key;
+    
+    var span = document.createElement('span');
+    span.textContent = data[key].name;
+    
+    drumEl.appendChild(h2);
+    drumEl.appendChild(span);
+    drumkit.appendChild(drumEl);
+    
+    data[key].el = drumEl;
+    drumEl.addEventListener('click', function(event) {
+      var key = event.currentTarget.querySelector('h2').textContent;
+      playDrum(key);
+    });
+  }
+};
+
+function playDrum(key) {
+  var audio = new Audio();
+  audio.src = data[key].sound;
+  audio.play();
+  
+  data[key].el.style.webkitAnimation ='drum-animation 0.3s';
+  data[key].el.style.animation = 'drum-animation 0.3s';
+  
+  data[key].el.addEventListener('webkitAnimationEnd', removeAnimation);
+  data[key].el.addEventListener('webkitAnimationEnd', removeAnimation);
+};
+
+function removeAnimation(event) {
+  event.currentTarget.style.webkitAnimation = 'none';
+  event.currentTarget.style.animation = 'none';
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+function handleKeyEvents(event) {
+  playDrum(event.key.toUpperCase());
+}
+
+
+construct();
+window.addEventListener('keydown', handleKeyEvents);
